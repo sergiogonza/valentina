7a5d748cb06f4cb69131c3379b2f45a4f007b5d6
sergiogonza <sergiogonzawr@gmail.com>
2025-04-26 23:43:09 -0500

    Update AboutText.js

--- components/About/AboutText.js
+++ components/About/AboutText.js
@@ -1,32 +1,24 @@
-
-
-
-
 function AboutText() {
-
   return (
-    // <div>
     <div
       className="md:w-1/2 w-full text-lg font-normal z-10 sticky md:mt-[10%] mt-[65vh] top-0 md:self-start text-body"
     >
-      Welcome to Chicity, where style meets substance in the heart of American
-      fashion. Born from a passion for timeless elegance and a commitment to
-      quality, our brand is a celebration of individuality and confidence.{" "}
-      <br />
-      At Chicity, we believe that fashion is more than just clothing—it's a
-      statement, an expression of self. Rooted in the vibrant spirit of the USA,
-      our collections blend classic sophistication with a contemporary edge,
-      ensuring you always step out in style. <br />
-      From the bustling streets of New York to the sun-soaked beaches of
-      California, our pieces are designed to accompany you on every journey,
-      making a lasting impression wherever you go. Join us in embracing the
-      essence of American fashion, where every stitch tells a story and every
-      outfit is an opportunity to showcase your unique flair. <br />
-      Welcome to Chicity—where fashion is not just about what you wear, but how
-      you wear it. Elevate your style, embody your essence, and make a statement
-      with us.
+      Bienvenido a Valentina López Arango Consultoría, donde la estrategia se
+      encuentra con la visión en un mundo de incertidumbre. Nacidos de la pasión
+      por la expansión empresarial y la capacidad para navegar la volatilidad
+      global, somos tu socio en la creación de oportunidades donde otros ven
+      riesgos. <br />
+      En Valentina López Arango, creemos que la estrategia empresarial es más
+      que solo números: es una visión, un plan de crecimiento y sostenibilidad.
+      Con base en el dinámico panorama geopolítico, nuestro enfoque combina
+      innovación con regulación, asegurando que siempre estés un paso adelante en
+      mercados inciertos. <br />
+      Desde la navegación de acuerdos comerciales hasta la anticipación de
+      cambios económicos, diseñamos estrategias que transforman los desafíos en
+      oportunidades. Únete a nosotros en la construcción de resiliencia y
+      expansión de tu negocio frente a la inestabilidad global. Cada decisión es
+      un paso hacia un futuro de crecimiento. <br />
     </div>
-    // </div>
   );
 }
 
